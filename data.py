db81f4c1c9c0ac22bfd3713721a9f475a8bd7c49
Joel <joelrsamaniego@gmail.com>
2026-08-30 09:38:15 +0800

    Updated Student ID format

--- data.py
+++ data.py
@@ -1,25 +1,30 @@
 import json
-import uuid
+from datetime import datetime
 from pathlib import Path
 STUDENT_DATA_FILE_NAME = "studentData.json"
 STUDENT_DATA_FILE_PATH = Path(STUDENT_DATA_FILE_NAME)
 
-def generateStudentID(studentNameParam):
-    return(uuid.uuid5(uuid.NAMESPACE_DNS, studentNameParam))
+def getFileContents():
+    fileContents = []
+    if STUDENT_DATA_FILE_PATH.is_file(): # Checks if the file exists
+        if STUDENT_DATA_FILE_PATH.stat().st_size != 0: # Checks if the file is not empty
+            with open(STUDENT_DATA_FILE_NAME, "r") as file:
+                fileContents = json.load(file)
+    return fileContents
 
-def getStudentDictionary(studentNameParam, studentProgramParam):
+def generateStudentID():
+    data = getFileContents()
+    return (datetime.now().year, 1000+len(data)+1)
+
+def getStudentDictionary(studentNameParam, studentProgramParam):    
     return {
         "Name": studentNameParam,
-        "Student ID": str(generateStudentID(studentNameParam)),
+        "Student ID": generateStudentID(),
         "Program": studentProgramParam,
     }
 
 def addToFile(studentParam):
-    existingData = []
-    if STUDENT_DATA_FILE_PATH.is_file(): # Checks if the file exists
-        if STUDENT_DATA_FILE_PATH.stat().st_size != 0: # Checks if the file is not empty
-            with open(STUDENT_DATA_FILE_NAME, "r") as file:
-                existingData = json.load(file)
-    existingData.append(studentParam)
+    data = getFileContents()
+    data.append(studentParam)
     with open("studentData.json", "w") as file:
-        json.dump(existingData, file, indent=4)
+        json.dump(data, file, indent=4)
